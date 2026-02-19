86ca4765e43342e76cd81a3b61f2ce822915d056
김경성님/디지털마케팅 <P199850@sktelecom.com>
2026-02-19 17:05:58 +0900

    react-router done

--- react-router/eslint.config.js
+++ react-router/eslint.config.js
@@ -10,20 +10,17 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+      reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
 ])
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { AppProvider } from '@/app/AppProvider'
+import AppRouter from '@/app/AppRouter'
+import '@/styles/global.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppProvider>
+      <AppRouter />
+    </AppProvider>
   </StrictMode>,
 )
