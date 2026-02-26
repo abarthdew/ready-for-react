02fc3fc462d34f04fbc4bfd0db08d3388b3fb8df
김경성님/디지털마케팅 <P199850@sktelecom.com>
2026-02-26 17:32:25 +0900

    / console.log()

--- react-hook/src/components/ExpensiveInsightsPanel.jsx
+++ react-hook/src/components/ExpensiveInsightsPanel.jsx
@@ -4,10 +4,10 @@ console.log('component render')
 function calculateInsights(size) {
   let score = 0
   for (let i = 0; i < 200000; i += 1) { 
-    console.log(size, i)
+    // console.log(size, i)
     score += (i * 13) % 7
   }
-  console.log(score, score % 10)
+  // console.log(score, score % 10)
   return { score: score % 10, recommendation: size > 3 ? 'Split by domain' : 'Keep simple' }
 }
 
